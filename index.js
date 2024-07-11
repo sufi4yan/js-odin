157bf0e8a0bd7fa3f6f6c0f23bcfb9d9e3534267
sufi4yan <sufiyanmasood598@gmail.com>
2024-07-11 13:55:01 +0500

    Checking new designs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 
-    const myLibrary = JSON.parse(localStorage.getItem(`library`)) || []
+const myLibrary = JSON.parse(localStorage.getItem(`library`)) || []
 let submit = document.getElementById(`submit-book`)
 let bookTitle = document.getElementById(`title`)
 let authorName = document.getElementById(`author-name`)
@@ -33,9 +33,11 @@ let authordisp = document.createElement(`div`)
 let ratingdisp = document.createElement(`div`)
 let right = document.createElement(`div`)
 let delbut = document.createElement(`button`)
+let editbut = document.createElement(`button`)
 //// setting relevant classes
 
 bookCont.classList.add(`book`)
+bookCont.setAttribute(`id`, `bookcont`)
 bookCont.classList.add(`anim`)
 left.classList.add(`left`)
 bookimg.classList.add(`book-prev`)
@@ -47,6 +49,10 @@ bookimg.setAttribute(`src`, `profile-pic.jpg`)
 right.classList.add(`right`)
 delbut.classList.add(`buttondel`)
 delbut.classList.add(`mama`)
+editbut.classList.add(`buttondel`)
+editbut.classList.add(`edit`)
+editbut.classList.add(`edite`)
+
 
 // appending to relevant parents
 
@@ -55,15 +61,19 @@ text.appendChild(titledisp)
 text.appendChild(authordisp)
 text.appendChild(ratingdisp)
 left.appendChild(text)
+right.appendChild(editbut)
 right.appendChild(delbut)
 bookCont.appendChild(left)
 bookCont.appendChild(right)
 console.log(myLibrary)
+
+
 function all(state) {
+    document.getElementById(`library`).innerHTML = ``
     num = 0
     function book(author, title, pages, hasRead, rating, ind) {
-    this.author = author,
     this.title = title,
+    this.author = author,
     this.pages = pages, 
     this.hasRead = hasRead,
     this.ratings = ratings,
@@ -96,10 +106,10 @@ function addtoLibrary() {
         authordisp.textContent = authorName.value
         ratingdisp.textContent = `${ratings} stars`
         
-        console.log(document.getElementById(`library`).appendChild(bookCont))
+        console.log(document.getElementById(`library`).appendChild(bookCont.cloneNode(true)))
         num += 1
         console.log(myLibrary)
-
+        location.reload()
     }
     else{
         alert(`This book already exists!`)
@@ -118,7 +128,7 @@ function hello() { myLibrary.forEach((item) => {
         authordisp.textContent = item.author
         ratingdisp.textContent = `${item.ratings} stars`
         delbut.setAttribute(`id`, `deleteBook${num}`)
-        
+        editbut.setAttribute(`id`, `editBook${num}`)
         document.getElementById(`library`).appendChild(bookCont.cloneNode(true))
         console.log(document.getElementById(`library`).innerHTML)
 
@@ -149,7 +159,7 @@ document.querySelectorAll(`.mama`).forEach((item) => {
         for (let i = 0; i < myLibrary.length; i++){
             myLibrary[i].ind = i
         }
-
+        
         localStorage.setItem(`library`, JSON.stringify(myLibrary))
         console.log(myLibrary)
 
@@ -160,6 +170,79 @@ document.querySelectorAll(`.mama`).forEach((item) => {
     })
 })
 
+document.querySelectorAll('.edite').forEach((item) => {
+    console.log(`h`)
+    item.addEventListener(`click`, () => {
+        console.log(item.id[item.id.length - 1])
+        let itemid = item.id[item.id.length - 1]
+
+        let bookContrep = document.createElement(`div`)
+        let leftrep = document.createElement(`div`)
+        let bookimgrep = document.createElement(`img`)
+        let textrep = document.createElement(`div`)
+        let titledisprep = document.createElement(`input`)
+        let authordisprep = document.createElement(`input`)
+        let ratingdisprep = document.createElement(`input`)
+        let cross = document.createElement(`button`)
+        let rightrep = document.createElement(`div`)
+        let checkbo = document.createElement(`input`)
+        let editsub = document.createElement(`button`);
+        
+    
+        checkbo.setAttribute(`type`, `checkbox`)
+        checkbo.setAttribute(`id`, `checkbo`)
+
+        leftrep.appendChild(bookimgrep)
+        
+        textrep.appendChild(titledisprep)
+        textrep.appendChild(authordisprep)
+        textrep.appendChild(ratingdisprep)
+        textrep.appendChild(checkbo)
+        textrep.appendChild(editsub)
+        rightrep.appendChild(textrep)
+        
+        
+        bookContrep.appendChild(leftrep)
+        bookContrep.appendChild(rightrep)
+        bookContrep.appendChild(cross)
+    
+        console.log(myLibrary)
+        textrep.classList.add(`rightrep`)
+        bookContrep.classList.add(`book-rep`)
+        cross.classList.add(`cross`)
+        bookContrep.setAttribute(`id`, `bookcont-rep`)
+        bookContrep.classList.add(`anim`)
+        leftrep.classList.add(`leftrep`)
+        rightrep.classList.add(`rightrep`)
+        bookimgrep.classList.add(`book-prev`)
+        textrep.classList.add(`text`)
+        titledisprep.setAttribute(`value`, myLibrary[itemid].title)
+        titledisprep.setAttribute(`id`, `titledisplayed`)
+        authordisprep.setAttribute(`id`, `authordisplayed`)
+        ratingdisprep.setAttribute(`id`, `rating`)
+        authordisprep.setAttribute(`value`, myLibrary[itemid].author)
+        ratingdisprep.setAttribute(`value`, myLibrary[itemid].ratings)
+        bookimgrep.setAttribute(`src`, `profile-pic.jpg`)
+        console.log(`hello`)
+        document.querySelector(`body`).appendChild(bookContrep)
+        editsub.classList.add(`submit-edit`)
+        cross.addEventListener(`click`, () => {
+            bookContrep.remove()
+        })
+        editsub.addEventListener(`click`, () => {
+            const confir = confirm(`Do you want to commit the changes to the book?`)
+            if (confir) {
+                myLibrary[itemid].author = authordisprep.value
+                console.log(myLibrary[itemid].author)
+                console.log(myLibrary[itemid])
+                localStorage.setItem(`library`, JSON.stringify(myLibrary))
+                all()
+                bookContrep.remove()
+            }
+        })
+    })
+        
+})
 
 
 
@@ -171,8 +254,6 @@ form.onsubmit = function() {
     event.preventDefault()
     addtoLibrary()
 }
-
-
 }
 function rating(which) {
     which === `onestar` ? which = 1: which === `twostar` ? which = 2 : which === `threestar` ? which = 3: which === `fourstar` ? which = 4: which === `fivestar` ? which = 5:null 
